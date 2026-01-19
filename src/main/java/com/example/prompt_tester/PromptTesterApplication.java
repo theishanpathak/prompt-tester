6d327048e662a56f364d00953dd2df3df65862a6
ipathak <ipathak@cs.olemiss.edu>
2026-01-19 00:24:45 -0600

    Initial Spring Boot setup with Thymeleaf template and index page

---
+++ src/main/java/com/example/prompt_tester/PromptTesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.prompt_tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prompt_tester/PromptTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prompt_tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
